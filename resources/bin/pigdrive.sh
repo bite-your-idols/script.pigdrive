a5e3fe93becad2fc5c8f3d56d8034aca0632d24c
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2017-02-05 09:18:38 +0100

    gPhotos

--- resources/bin/pigdrive.sh
+++ resources/bin/pigdrive.sh
@@ -12,13 +12,20 @@ USER_SETTINGS=$(cat $SETTINGS_FILE)
 #     <setting id="localpath" value="/storage/downloads/" />
 #     <setting id="torrentcatcher" value="true" />
 #     <setting id="torrentpath" value="/path/to/my/watchlist/" />
-# </settings>
+#     <setting id="photoscloudpath" value="" />
+#     <setting id="photoslocalpath" value="/storage/picture/"/>
+#     <setting id="photosbackup" value="false"/>
+# </settings>	
 AUTOSTART=$(echo $USER_SETTINGS | sed -e 's~.*id="autostart" value="\(.*\)" /> <setting id="cloudpath.*~\1~')
 CLOUD_FOLDER=$(echo $USER_SETTINGS | sed -e 's~.*id="cloudpath" value="\(.*\)" /> <setting id="localpath.*~\1~')
 LOCAL_FOLDER=$(echo $USER_SETTINGS | sed -e 's~.*id="localpath" value="\(.*\)" /> <setting id="torrentcatcher.*~\1~')
 TORRENT_CATCHER=$(echo $USER_SETTINGS | sed -e 's~.*id="torrentcatcher" value="\(.*\)" /> <setting id="torrentpath.*~\1~')
 # si es false la anterior esto deberia quedarse vacio, porque al ser false no se metido en el user settings
-TORRENT_FOLDER=$(echo $USER_SETTINGS | sed -e 's~.*id="torrentpath" value="\(.*\)/" /> </settings>.*~\1~')
+#TORRENT_FOLDER=$(echo $USER_SETTINGS | sed -e 's~.*id="torrentpath" value="\(.*\)/" /> </settings>.*~\1~')
+TORRENT_FOLDER=$(echo $USER_SETTINGS | sed -e 's~.*id="torrentpath" value="\(.*\)/" /> <setting id="photoscloudpath.*~\1~')
+PHOTOS_CLOUD_FOLDER=$(echo $USER_SETTINGS | sed -e 's~.*id="photoscloudpath" value="\(.*\)" /> <setting id="photoslocalpath.*~\1~')
+PHOTOS_LOCAL_FOLDER=$(echo $USER_SETTINGS | sed -e 's~.*id="photoslocalpath" value="\(.*\)" /> <setting id="photosbackup.*~\1~')
+PHOTOS_CATCHER=$(echo $USER_SETTINGS | sed -e 's~.*id="photosbackup" value="\(.*\)/" /> </settings>.*~\1~')
 
 echo "Action -> "$ACTION >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
 echo "Autostart -> "$AUTOSTART >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
@@ -26,6 +33,9 @@ echo "Cloud Folder -> "$CLOUD_FOLDER >> /storage/.kodi/userdata/addon_data/scrip
 echo "Local Folder -> "$LOCAL_FOLDER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
 echo "Torrent Catcher -> "$TORRENT_CATCHER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
 echo "Torrent Watchfolder -> "$TORRENT_FOLDER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+echo "Photos Cloud Folder -> "$PHOTOS_CLOUD_FOLDER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+echo "Photos Local Folder -> "$PHOTOS_LOCAL_FOLDER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+echo "Photos Catcher -> "$PHOTOS_CATCHER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
 
 #entramos en la carpeta donde tenemos el drive
 cd $LOCAL_FOLDER
@@ -66,6 +76,14 @@ case $ACTION in
 	# "torrents")  
 	# 	# forzamos coger los torrent desde settings 
 	# ;;
+	
+	"photos")  
+    	# forzamos descargar photos desde settings 
+    	echo "Photos" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+    	
+    	kodi-send --action=Notification"(PiGDrive,Goolge Photos download complete,2000,/storage/.kodi/addons/script.pigdrive/icon.png)"
+	;;
+	
 
 	*)  
 		if [ $AUTOSTART == 'true' ] || [ $ACTION == "sync" ]; then
@@ -83,9 +101,21 @@ case $ACTION in
 				mv *.torrent "$TORRENT_FOLDER"
 				cd $LOCAL_FOLDER
 			fi 
+			
+			# checkeamos que tiene marcado google photos en true
+			if [ $PHOTOS_CATCHER == 'true' ]; then
+				echo "Google Photos" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+				#entramos a la carpeta de destino, por si no es la raiz, para mover los torrent a la carpeta del transmission
+				# echo "movemos los torrents"
+				
+				#cd "$LOCAL_FOLDER/$PHOTOS_CLOUD_FOLDER"
+				# $DRIVE_FILE pull -files --no-clobber --exclude-ops delete -quiet -depth 2 "$PHOTOS_CLOUD_FOLDER" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+				#mv "$LOCAL_FOLDER/$PHOTOS_CLOUD"_FOLDER "$PHOTOS_CLOUD_FOLDER"
+				#cd $LOCAL_FOLDER
+			fi 
 
 			#subimos a drive lo que queda aqui, 
-			$DRIVE_FILE push -files -quiet -depth 2 "$CLOUD_FOLDER" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+			#$DRIVE_FILE push -files -quiet -depth 2 "$CLOUD_FOLDER" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
 
 		fi
   	;;
