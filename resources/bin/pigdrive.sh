8e345f0cd4f0fcbf6cd48b4112e3a534fa12084d
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2017-06-12 23:46:48 +0200

    sync priority pulled data, remove pi files not in cloud

--- resources/bin/pigdrive.sh
+++ resources/bin/pigdrive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #documentation: https://github.com/odeke-em/drive
-echo '::PiGDrive:: -> sync on ' $(date) &> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+echo '::PiGDrive:: -> sync on ' $(date) &> /storage/.kodi/temp/pigdrive.log
 #defining variables
 ACTION=$1
 DRIVE_FILE="/storage/.kodi/addons/script.pigdrive/resources/bin/drive"
@@ -29,15 +29,15 @@ TORRENT_CATCHER=$(echo $USER_SETTINGS | sed -e 's~.*id="torrentcatcher" value="\
 TORRENT_FOLDER=$(echo $USER_SETTINGS | sed -e 's~.*id="torrentpath" value="\(.*\)/" /> </settings>.*~\1~')
 
 
-echo "Action -> "$ACTION >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
-echo "Autostart -> "$AUTOSTART >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
-echo "Cloud Folder -> "$CLOUD_FOLDER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
-echo "Local Folder -> "$LOCAL_FOLDER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
-echo "Torrent Catcher -> "$TORRENT_CATCHER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
-echo "Torrent Watchfolder -> "$TORRENT_FOLDER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
-echo "Photos Cloud Folder -> "$PHOTOS_CLOUD_FOLDER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
-echo "Photos Local Folder -> "$PHOTOS_LOCAL_FOLDER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
-echo "Photos Catcher -> "$PHOTOS_CATCHER >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+echo "Action -> "$ACTION >> /storage/.kodi/temp/pigdrive.log
+echo "Autostart -> "$AUTOSTART >> /storage/.kodi/temp/pigdrive.log
+echo "Cloud Folder -> "$CLOUD_FOLDER >> /storage/.kodi/temp/pigdrive.log
+echo "Local Folder -> "$LOCAL_FOLDER >> /storage/.kodi/temp/pigdrive.log
+echo "Torrent Catcher -> "$TORRENT_CATCHER >> /storage/.kodi/temp/pigdrive.log
+echo "Torrent Watchfolder -> "$TORRENT_FOLDER >> /storage/.kodi/temp/pigdrive.log
+echo "Photos Cloud Folder -> "$PHOTOS_CLOUD_FOLDER >> /storage/.kodi/temp/pigdrive.log
+echo "Photos Local Folder -> "$PHOTOS_LOCAL_FOLDER >> /storage/.kodi/temp/pigdrive.log
+echo "Photos Catcher -> "$PHOTOS_CATCHER >> /storage/.kodi/temp/pigdrive.log
 
 
 function getTorrents {
@@ -51,7 +51,7 @@ function getTorrents {
 function getPhotos {
 	# opcion 1 -> bajamos todo y lo pasamos a la carpeta
  	# echo "las acciones del backup de gPhotos"
- 	# $DRIVE_FILE pull --no-clobber --exclude-ops delete -quiet -depth -1 "$PHOTOS_CLOUD_FOLDER" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+ 	# $DRIVE_FILE pull --no-clobber --exclude-ops delete -quiet -depth -1 "$PHOTOS_CLOUD_FOLDER" >> /storage/.kodi/temp/pigdrive.log
  	# $echo $DRIVE_FILE pull --no-clobber --exclude-ops delete  -depth -1 "$PHOTOS_CLOUD_FOLDER"
 	# despues de bajar la carpeta fotos la movemos al directio local deseado
 	# mkdir "$PHOTOS_LOCAL_FOLDER$PHOTOS_CLOUD_FOLDER"
@@ -74,7 +74,7 @@ function getPhotos {
 	    ln -s "$PHOTOS_LOCAL_FOLDER$PHOTOS_CLOUD_FOLDER" "$LOCAL_FOLDER$PHOTOS_CLOUD_FOLDER" 
 	fi
 
-	$echo $DRIVE_FILE pull --no-clobber --exclude-ops delete -quiet -depth -1 "$PHOTOS_CLOUD_FOLDER" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+	$echo $DRIVE_FILE pull --no-clobber --exclude-ops delete -quiet -depth -1 "$PHOTOS_CLOUD_FOLDER" >> /storage/.kodi/temp/pigdrive.log
 
 }
 
@@ -84,7 +84,7 @@ cd $LOCAL_FOLDER
 case $ACTION in
 	"setup")
 		# PiGDrive installation setup
-		echo "Setup" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+		echo "Setup" >> /storage/.kodi/temp/pigdrive.log
 		chmod a+x $DRIVE_FILE
 		$DRIVE_FILE init "$LOCAL_FOLDER"
 
@@ -97,32 +97,32 @@ case $ACTION in
 		echo '( sleep 1m && sh /storage/.kodi/addons/script.pigdrive/resources/bin/pigdrive.sh ) &' >> /storage/.config/autostart.sh
 
 		$DRIVE_FILE quota
-		$DRIVE_FILE quota >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+		$DRIVE_FILE quota >> /storage/.kodi/temp/pigdrive.log
 	;;
 
 	"upload")  
     	# forzamos el push desde settings 
-    	echo "Upload" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
-    	$DRIVE_FILE push -files -quiet -depth 2 "$CLOUD_FOLDER" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+    	echo "Upload" >> /storage/.kodi/temp/pigdrive.log
+    	$DRIVE_FILE push -files -quiet -depth 2 "$CLOUD_FOLDER" >> /storage/.kodi/temp/pigdrive.log
     	kodi-send --action=Notification"(PiGDrive,Local pushed to Drive,2000,/storage/.kodi/addons/script.pigdrive/icon.png)"
 	;;
 
 	"download")  
     	# forzamos el pull desde settings 
-    	echo "Download" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
-    	$DRIVE_FILE pull -files -quiet -depth 2 "$CLOUD_FOLDER" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+    	echo "Download" >> /storage/.kodi/temp/pigdrive.log
+    	$DRIVE_FILE pull -files -quiet -depth 2 "$CLOUD_FOLDER" >> /storage/.kodi/temp/pigdrive.log
     	kodi-send --action=Notification"(PiGDrive,Drive pulled to local,2000,/storage/.kodi/addons/script.pigdrive/icon.png)"
 	;;
 
 	"torrents")  
-		echo "Torrents" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+		echo "Torrents" >> /storage/.kodi/temp/pigdrive.log
 	 	# forzamos coger los torrent desde settings 
 		getTorrents
 	;;
 	
 	"photos")  
     	# forzamos descargar photos desde settings 
-    	echo "Photos" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+    	echo "Photos" >> /storage/.kodi/temp/pigdrive.log
     	getPhotos
     	kodi-send --action=Notification"(PiGDrive,Goolge Photos download complete,2000,/storage/.kodi/addons/script.pigdrive/icon.png)"
 	;;
@@ -131,30 +131,32 @@ case $ACTION in
 	*)  
 		if [ $AUTOSTART == 'true' ] || [ $ACTION == "sync" ]; then
 			# lo que se ejecuta por defecto al arrancar kodi o al activar torrent
-			echo "Sync" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+			echo "Sync" >> /storage/.kodi/temp/pigdrive.log
 			
-			$DRIVE_FILE pull -files --no-clobber --exclude-ops delete -quiet -depth 2 "$CLOUD_FOLDER" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+			# $DRIVE_FILE pull -files --no-clobber --exclude-ops delete -quiet -depth 2 "$CLOUD_FOLDER" >> /storage/.kodi/temp/pigdrive.log
+			$DRIVE_FILE pull -files --no-clobber -quiet -depth 2 "$CLOUD_FOLDER" >> /storage/.kodi/temp/pigdrive.log
+
 
 			# checkeamos que tiene marcado torrentcatcher en true
 			if [ $TORRENT_CATCHER == 'true' ]; then
-				echo "Torrent" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+				echo "Torrent" >> /storage/.kodi/temp/pigdrive.log
 				getTorrents
 			fi 
 			
 			# checkeamos que tiene marcado google photos en true
 			if [ $PHOTOS_CATCHER == 'true' ]; then
-				echo "Google Photos" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+				echo "Google Photos" >> /storage/.kodi/temp/pigdrive.log
 				getPhotos
 			fi 
 
 			#subimos a drive lo que queda aqui, 
-			$DRIVE_FILE push -files -quiet -depth 2 "$CLOUD_FOLDER" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+			# $DRIVE_FILE push -files -quiet -depth 2 "$CLOUD_FOLDER" >> /storage/.kodi/temp/pigdrive.log
 
 		fi
   	;;
 esac
 
-echo "Finished" >> /storage/.kodi/userdata/addon_data/script.pigdrive/pigdrive.log
+echo "Finished" >> /storage/.kodi/temp/pigdrive.log
 
 # EJEMPLOS
 
