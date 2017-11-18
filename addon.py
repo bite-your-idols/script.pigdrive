308bbd8a174df539976f0e9da2b63e55a15fb5db
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2017-11-18 12:16:28 +0100

    Add files via upload

--- addon.py
+++ addon.py
@@ -7,17 +7,6 @@ import sys
 import os.path
 import urlparse
  
-# addon       = xbmcaddon.Addon()
-# addonname   = addon.getAddonInfo('name')
-# addonid 	= addon.getAddonInfo('id')
-# script_file = os.path.realpath(__file__)
-# directory = os.path.dirname(script_file)
-
-# cloudPath = xbmcplugin.getSetting(int(sys.argv[1]),'cloudpath')
-# torrentPath = xbmcplugin.getSetting(int(sys.argv[1]),'torrentpath')
-# localPath = xbmcplugin.getSetting(int(sys.argv[1]),'localpath')
-# torrentCatcher = xbmcplugin.getSetting(int(sys.argv[1]),'torrentCatcher')
-
 count = len(sys.argv) - 1
  
 if count > 0:
@@ -39,29 +28,6 @@ else:
 # command = args['com'][0] if 'com' in args else 'EXEC_ADDON'
 # xbmcgui.Dialog().ok("PiGDrive",  command)
 # command = "FOOO"
-
-
-# GET SETTINGS 
-# addon       = xbmcaddon.Addon()
-# addonname   = addon.getAddonInfo('name')
-# addonid 	= addon.getAddonInfo('id')
-# script_file = os.path.realpath(__file__)
-# directory = os.path.dirname(script_file)
-
-# cloudPath = xbmcaddon.Addon(id='script.pigdrive').getSetting('cloudpath')
-# torrentPath = xbmcaddon.Addon(id='script.pigdrive').getSetting('torrentpath')
-# localPath = xbmcaddon.Addon(id='script.pigdrive').getSetting('localpath')
-# torrentCatcher = xbmcaddon.Addon(id='script.pigdrive').getSetting('torrentCatcher')
-
-#xbmcaddon.Addon(id='script.pigdrive').openSettings()
-
-# import xbmcaddon
-# import xbmcgui
-
-# variable= xbmcaddon.Addon(id='script.pigdrive').getSetting('autostart')
-#xbmcgui.Dialog().ok("TEST", variable)
-
-
 
 if command == 'SETUP':
 	xbmcgui.Dialog().ok("PiGDrive", "To install Google Drive you need to connect to your RPi using ssh, exec this command and follow the instructions: 'sh /storage/.kodi/addons/script.pigdrive/resources/bin/pigdrive.sh setup'")
@@ -85,6 +51,62 @@ elif command == 'TORRENTS':
 elif command == 'PHOTOS':
 	xbmcgui.Dialog().ok("PiGDrive", "Do you want to download/backup your Google Photos to local storage?")
 	os.system("sh /storage/.kodi/addons/script.pigdrive/resources/bin/pigdrive.sh photos")
- 
+
+
+# GET SETTINGS 
+#  kodi-send --action="RunScript(script.pigdrive, SETTINGS)"
+
+elif command == 'SETTINGS':
+	autostart = xbmcaddon.Addon(id='script.pigdrive').getSetting('autostart')
+	cloudPath = xbmcaddon.Addon(id='script.pigdrive').getSetting('cloudpath')
+	localPath = xbmcaddon.Addon(id='script.pigdrive').getSetting('localpath')
+	torrentPath = xbmcaddon.Addon(id='script.pigdrive').getSetting('torrentpath')
+	torrentCatcher = xbmcaddon.Addon(id='script.pigdrive').getSetting('torrentCatcher')
+	photoscloudpath = xbmcaddon.Addon(id='script.pigdrive').getSetting('photoscloudpath')
+	photoslocalpath = xbmcaddon.Addon(id='script.pigdrive').getSetting('photoslocalpath')
+	photosbackup = xbmcaddon.Addon(id='script.pigdrive').getSetting('photosbackup')
+
+	# msg="autostart:'"+autostart+"'\ncloudPath:'"+cloudPath+"'\nlocalPath:'"+localPath+"'\ntorrentPath:'"+torrentPath+"'\ntorrentCatcher:'"+torrentCatcher+"'\nphotoscloudpath:'"+photoscloudpath+"'\nphotoslocalpath:'"+photoslocalpath+"'\nphotosbackup:'"+photosbackup+"'"
+	msg=autostart+"\n"+cloudPath+"\n"+localPath+"\n"+torrentPath+"\n"+torrentCatcher+"\n"+photoscloudpath+"\n"+photoslocalpath+"\n"+photosbackup
+	f = open( '/storage/.kodi/userdata/addon_data/script.pigdrive/settings.txt', 'w' )
+
+	f.write( msg)
+	f.close()
+
+# elif command == 'SETTINGS_AUTOSTART':
+# 	msg= xbmcaddon.Addon(id='script.pigdrive').getSetting('autostart')
+# 	xbmcgui.Dialog().ok("Settings", msg)
+
+# elif command == 'SETTINGS_CLOUDPATH':
+# 	msg= xbmcaddon.Addon(id='script.pigdrive').getSetting('cloudPath')
+# 	xbmcgui.Dialog().ok("Settings", msg)
+
+# elif command == 'SETTINGS_LOCALPATH':
+# 	msg= xbmcaddon.Addon(id='script.pigdrive').getSetting('localPath')
+# 	xbmcgui.Dialog().ok("Settings", msg)
+# 	print(msg)
+
+# elif command == 'SETTINGS_TORRENTPATH':
+# 	msg= xbmcaddon.Addon(id='script.pigdrive').getSetting('torrentPath')
+# 	xbmcgui.Dialog().ok("Settings", msg)
+
+# elif command == 'SETTINGS_TORRENTCATCHER':
+# 	msg= xbmcaddon.Addon(id='script.pigdrive').getSetting('torrentCatcher')
+# 	xbmcgui.Dialog().ok("Settings", msg)
+
+# elif command == 'SETTINGS_PHOTOSCLOUDPATH':
+# 	msg= xbmcaddon.Addon(id='script.pigdrive').getSetting('photoscloudpath')
+# 	xbmcgui.Dialog().ok("Settings", msg)
+
+# elif command == 'SETTINGS_PHOTOSLOCALPATH':
+# 	msg= xbmcaddon.Addon(id='script.pigdrive').getSetting('photoslocalpath')
+# 	xbmcgui.Dialog().ok("Settings", msg)
+
+# elif command == 'SETTINGS_PHOTOSBACKUP':
+# 	msg= xbmcaddon.Addon(id='script.pigdrive').getSetting('photosbackup')
+# 	xbmcgui.Dialog().ok("Settings", msg)
+
+
+# SINO ES NADA DE ESO LANZAMOS EL ADDON
 else:
 	xbmcaddon.Addon(id='script.pigdrive').openSettings()
